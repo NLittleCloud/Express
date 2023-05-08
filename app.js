6cb4bd1839786cc516d1f1235d5ab36f8a9a389c
Вероника Кузнецова <you@example.com>
2023-05-08 18:58:16 +0300

    LR 7

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const express = require('express');
-const {v1_router, v1_router2} = require("./router/router");
+const v1_router = require("./router/router");
+const morgan = require("morgan");
+const helmet = require('helmet')
+
 const HOST = '127.0.0.1';
 const PORT = 8000;
-
 const app = express();
+
+app.use(morgan('tiny'));
+app.use(helmet());
 app.use(express.static('public'));
 
 app.use('/v1', v1_router);
-app.use('/v1/comments', v1_router2);
+
+
+app.use(function(err, req, res, next) {
+
+	if(err.statusCode)
+	{
+		res.status(err.statusCode).json(err.message);
+	}else{
+		res.status(400).json("Отправьте запрос корректно!");
+	}
+});
 
 app.listen(PORT, HOST, () =>{
 	console.log(`Сервер запущен http://${HOST}:${PORT}`);
-});
-
-app.use((req, res) => {
-	res.status(404).send('Данная страница не найдена!');
-});
-
-app.use((err, req,res) => {
-	res.status(500).send('Ошибка сервера')
 });
--- router/router.js
+++ router/router.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const v1_router = express.Router();
-const v1_router2 = express.Router();
+
 const jsonParser = express.json();
-
-
 let user = {user_agent: 0};
 let com = " ";
+
+function validation(req, res, next){
+	if(JSON.stringify(req.body) == '{}'){
+		next(err);
+	}else next();
+}
+
+function checkAuthorization(req, res, next){
+	const apiKey = req.query.apiKey;
+	if (apiKey !== 'api'){
+		next(err);
+	}
+	else next();
+}
 
 v1_router.get('/', (req, res) => {
 	res.send('Рады видеть вас на нашем сервере!');
@@ -20,15 +32,22 @@ v1_router.get('/stats', (req, res) => {
 	</table>`) 
 });
 
+v1_router.post('/users', jsonParser, validation, checkAuthorization, (req, res) => {
+	res.send('Успешно!');
+});
 
-v1_router2.get('/', (req, res) => {
+v1_router.get('/comments', (req, res) => {
  	res.send(com);
 });
 
-v1_router2.post('/', jsonParser, (req, res) => {
+v1_router.post('/comments', jsonParser, validation, (req, res) => {
 	console.log(req.body);
 	com += JSON.stringify(req.body);
 	res.send('Спасибо, за вашу отзывчивость!');
 });
 
-module.exports = {v1_router, v1_router2};
+v1_router.use((req, res, next) =>{
+  res.status(404).send('Такой страницы не существует!');
+});
+
+module.exports = v1_router;
